ecc73f3bfe8a11832c936e248d4201aa489b38b7
Nix-code <nishantbanjade07@gmail.com>
2024-08-07 22:14:36 +0545

    remove dup

---
+++ leetcode/Easy/RemoveDuplicate.java
@@ -0,0 +1,24 @@
+public class RemoveDuplicate {
+    static int removeDuplicates(int[] nums) {
+       
+        int i = 0, j = 1;
+        int k = 1;
+        while(i < nums.length && j < nums.length){
+ 
+             if(nums[i] == nums[j]){
+                 j++;
+             }
+             else {
+                 nums[i+1] = nums[j];
+                 k++;
+                 i++;
+             }
+        }
+        return k;
+     }
+    public static void main(String[] args) {
+        int nums []= {0, 0, 0, 1, 1, 1, 2, 2, 3};
+
+        System.out.println(removeDuplicates(nums));
+    }
+}
