926879dded0351ace9f7cadc45412d331d904b2e
Nix-code <nishantbanjade07@gmail.com>
2024-08-07 22:28:52 +0545

    add alternate solution

--- leetcode/Easy/RemoveDuplicate.java
+++ leetcode/Easy/RemoveDuplicate.java
@@ -16,6 +16,18 @@ public class RemoveDuplicate {
         }
         return k;
      }
+    //  public int removeDuplicates(int[] nums) {
+       
+    //     int j=0;
+    //     for(int i = 1; i < nums.length; i++){
+    //          if(nums[i]!=nums[j]){
+    //              nums[j+1] = nums[i];
+    //              j++;
+                 
+    //          } 
+    //     }
+    //     return j+1;
+    //  }
     public static void main(String[] args) {
         int nums []= {0, 0, 0, 1, 1, 1, 2, 2, 3};
 
